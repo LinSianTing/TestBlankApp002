2c4728220b0d1e142ab15450b10c6ac9d6b6e763
Lin SianTing <siantingtaiwan@gmail.com>
2018-01-20 01:53:03 +0800

    Initial Commit

---
+++ BlankApp1/BlankApp1/ViewModels/MainPage02ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlankApp1.ViewModels
+{
+    public class MainPage02ViewModel : ViewModelBase
+    {
+        public MainPage02ViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page 02";
+        }
+    }
+}
---
+++ BlankApp1/BlankApp1/Views/MainPage02.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BlankApp1.Views
+{
+	public partial class MainPage02 : ContentPage
+	{
+		public MainPage02()
+		{
+			InitializeComponent ();
+        
+    }
+
+
+	}
+}
